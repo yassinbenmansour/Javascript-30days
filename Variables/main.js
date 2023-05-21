e64706f1d39268a60ce6f96609b9e4048a74fc20
yassinbenmansour <benmansour.yassinne@gmail.com>
2023-05-21 17:15:52 +0100

    update

---
+++ Variables/main.js
@@ -0,0 +1,13 @@
+let titre = "MAC OS"
+const price1 = 13000;
+const price2 = 15000;
+var frait = 120 ;
+let total = price1 + frait ;
+
+document.getElementById('titre').innerHTML=titre;
+document.getElementById('prix').innerHTML=price1 + "DH";
+document.getElementById('prixs').innerHTML=price2 + "DH";
+document.getElementById('total').innerHTML=total;
+
+
+
