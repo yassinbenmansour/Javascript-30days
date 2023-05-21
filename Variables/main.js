ac8afdcb229867db6bdc2ebda11d54658994c0a5
yassinbenmansour <benmansour.yassinne@gmail.com>
2023-05-21 21:11:03 +0100

    update

--- Variables/main.js
+++ Variables/main.js
@@ -11,3 +11,28 @@ document.getElementById('total').innerHTML=total;
 
 
 
+// Declaring different variables of different data types
+let firstName = 'yassine' // first name of a person
+let lastName = 'benmansour' // last name of a person
+let country = 'morocco' // country
+let city = 'sale' // capital city
+let age = 100 // age in years
+let isMarried = false
+
+// Declaring variables with number values
+const gravity = 9.81 // earth gravity  in m/s2
+const boilingPoint = 100 // water boiling point, temperature in oC
+const PI = 3.14 // geometrical constant
+
+// Variables can also be declaring in one line separated by comma
+let name = 'yassine', //name of a person
+  job = 'freelancer',
+  live = 'morocco'
+
+
+console.log(firstName, lastName, country, city, age, isMarried) 
+console.log(gravity, boilingPoint, PI) // 9.81, 100, 3.14
+console.log(name, job, live)
+
+
+
