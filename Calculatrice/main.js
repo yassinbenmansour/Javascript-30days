d08941b2b57eaaac6611d5bbed262e21a490f4e7
yassinbenmansour <benmansour.yassinne@gmail.com>
2023-05-21 20:35:47 +0100

    calcul

--- Calculatrice/main.js
+++ Calculatrice/main.js
@@ -1,23 +1,29 @@
-function calculate() {
-        var num1 = parseInt(document.getElementById("num1").value);
-        var num2 = parseInt(document.getElementById("num2").value);
-  
-        var operation = document.getElementById("operation").value;
-        var result;
-  
-        if (operation === "add") {
-          result = num1 + num2;
-        } else if (operation === "subt") {
-          result = num1 - num2;
-        } else if (operation === "mul") {
-          result = num1 * num2;
-        } else if (operation === "div") {
-          if (num2 === 0) {
-            result = "Error: Division by zero";
-          } else {
-            result = num1 / num2;
-          }
-        }
-  
+function add(){
+        var nbr1 = parseInt(document.getElementById("nbr1").value)
+        var nbr2 = parseInt(document.getElementById("nbr2").value)
+        var result = nbr1 + nbr2 
         document.getElementById("result").innerHTML = "Result: " + result;
-      }
+}
+
+function sous(){
+        var nbr1 = parseInt(document.getElementById("nbr1").value)
+        var nbr2 = parseInt(document.getElementById("nbr2").value)
+        var result = nbr1 - nbr2 
+        document.getElementById("result").innerHTML = "Result: " + result;
+}
+
+
+function multi(){
+        var nbr1 = parseInt(document.getElementById("nbr1").value)
+        var nbr2 = parseInt(document.getElementById("nbr2").value)
+        var result = nbr1 * nbr2 
+        document.getElementById("result").innerHTML = "Result: " + result;
+}
+
+
+function div(){
+        var nbr1 = parseInt(document.getElementById("nbr1").value)
+        var nbr2 = parseInt(document.getElementById("nbr2").value)
+        var result = nbr1 / nbr2 
+        document.getElementById("result").innerHTML = "Result: " + result;
+}
