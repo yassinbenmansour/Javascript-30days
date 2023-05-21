882738cd2beeaddaebdeae843c82e476c9d7cfc2
yassinbenmansour <benmansour.yassinne@gmail.com>
2023-05-21 21:17:34 +0100

    accessing char

---
+++ datatype/main.js
@@ -0,0 +1,8 @@
+let string = "benmansour"
+let firstletter = [0]
+console.log(firstletter) //=> b
+
+let lastindex = string.length - 1
+console.log(lastindex) // => 9
+console.log(string[lastindex]) // => r
+
