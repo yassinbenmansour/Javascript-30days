96ead3dd78b2adba99b79694dea5adf29d07a3ce
yassinbenmansour <benmansour.yassinne@gmail.com>
2023-05-21 22:17:23 +0100

    charAt

---
+++ datatype/chartAt.js
@@ -0,0 +1,14 @@
+// charAt(): Takes index and it returns the value at that index
+
+var string = "yassine benmansour"
+
+var index = string.charAt(0)
+console.log(index)  // y
+
+
+// char last index
+
+var lastindex = string.length - 1
+console.log(lastindex)  // 16
+console.log(string.charAt(lastindex)) // r
+
