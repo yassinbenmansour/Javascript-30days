25053bae348eafafeb196e13c1881fa619d4e241
yassinbenmansour <benmansour.yassinne@gmail.com>
2023-05-27 00:09:43 +0100

    update

---
+++ datatype/charCodeAt.js
@@ -0,0 +1,7 @@
+// charCodeAt(): Takes index and it returns char code(ASCII number) of the value at that index
+
+let string = 'yassine benmansour'
+console.log(string.charAt(3)) // s => ASSCI is 115
+console.log(string.charCodeAt(3)) // s => ASSCI is 115
+let lastIndex = string.length - 1
+console.log(string.charCodeAt(lastIndex)) // r ASCII is 114
