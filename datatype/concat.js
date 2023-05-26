322278446058b3851785310d813774ee85d9acd9
yassinbenmansour <benmansour.yassinne@gmail.com>
2023-05-27 00:12:36 +0100

    concat

---
+++ datatype/concat.js
@@ -0,0 +1,6 @@
+// concat(): it takes many substrings and creates concatenation.
+// string.concat(substring, substring, substring)
+let string = 'yassine'
+console.log(string.concat(" ","benmansour", " ", "software", " ","dev")) //yassine benmansour software dev
+let country = 'Fin'
+console.log(country.concat("land")) // Finland
