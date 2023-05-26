2872f9a6de4bb3aa7bb754d2f600f475775b6837
yassinbenmansour <benmansour.yassinne@gmail.com>
2023-05-27 00:46:00 +0100

    PUSH

---
+++ datatype/lowercase.js
@@ -0,0 +1,7 @@
+// toLowerCase(): this method changes the string to lowercase letters.
+let string = 'JavasCript'
+console.log(string.toLowerCase())     // javascript
+let firstName = 'YASSine'
+console.log(firstName.toLowerCase())  // yassine
+let country = 'Finland'
+console.log(country.toLowerCase())   // finland
---
+++ datatype/uppercase.js
@@ -0,0 +1,8 @@
+// toUpperCase(): this method changes the string to uppercase letters.
+
+let string = 'JavaScript'
+console.log(string.toUpperCase())      // JAVASCRIPT
+let firstName = 'yassine'
+console.log(firstName.toUpperCase())  // YASSINE
+let country = 'Finland'
+console.log(country.toUpperCase())    // FINLAND
